7f67d28e63fa64d1ddfabec9be302abfb1130de8
Brian Beck <exogen@gmail.com>
2017-11-04 19:37:07 -0700

    Update syntax

--- scripts/start.js
+++ scripts/start.js
@@ -1,7 +1,7 @@
 /**
  * Run a demo server with the extension enabled.
  */
-const start = require('graphbrainz').start
+const { start  }= require('graphbrainz')
 const extension = require('../src')
 
 start({ extensions: [extension] })

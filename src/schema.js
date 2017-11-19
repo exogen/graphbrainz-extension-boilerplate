a8875e59a1d4aedaddb34dc6800b5b3f733f4ec4
Brian Beck <exogen@gmail.com>
2017-11-18 16:51:44 -0800

    Use new gql tag in graphbrainz 7.3.0

--- src/schema.js
+++ src/schema.js
@@ -1,4 +1,6 @@
-module.exports = `
+const gql = require('graphbrainz/lib/tag').default
+
+module.exports = gql`
   extend type Artist {
     # A message that says hello to the artist.
     helloWorld: String!
